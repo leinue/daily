d9f575a53269d3fef65b072b1876b809c5be19e2
ivy <597055914@qq.com>
2014-09-15 07:28:32 +0800

    update api

--- api.php
+++ api.php
@@ -3,7 +3,18 @@
 <div class="zhihu-body">
 	<div class="api-main-panel">
 		<div class="api-main-text">
-			sdsdsd
+			<p>消息输出格式:JSON</p>
+			<p>HTTP Method:GET</p>
+			<p>获取最新消息:</p>
+			<p><strong>①http://news-at.zhihu.com/api/3/news/latest</strong></p>
+			<p>获取消息内容:</p>
+			<p><strong>②http://news-at.zhihu.com/api/3/news/+消息ID</strong></p>
+			<p>消息ID可由①获得</p>
+			<p>获取以往内容:</p>
+			<p><strong>③http://news.at.zhihu.com/api/3/news/before/+消息ID</strong></p>
+			<p>热门消息:</p>
+			<p><strong>④http://news-at.zhihu.com/api/3/news/hot</strong></p>
+			<p>GitHub项目地址:<a href="https://github.com/leinue/daily" target="_blank">https://github.com/leinue/daily</a></p>
 		</div>
 	</div>
 </div>
