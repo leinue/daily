6e77f47ee9c168a4a574b4f4c49fc460720f7d68
ivy <597055914@qq.com>
2014-09-23 09:27:43 +0800

    update a lot

--- beta/fun/cls.php
+++ beta/fun/cls.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" type="text/css" href="http://news.at.zhihu.com/css/news_qa.auto.css?v=0abbb">
 <?php
 
 class infoMgr{
@@ -94,22 +95,50 @@ class DataObj{
 	public $_data;
 	public $storiesData;
 	public $singleNew;
+	const _LIST='1';
+	const BODY='2';
+	public $method;
 
-	function __construct($data){$this->_data=$data;}
+	function __construct($data,$method){
+		$this->_data=$data;
+		$this->method=$method;
+	}
 
-	function getDate(){return $this->_data['date'];}
+	private function isList(){
+		if($this->method==self::_LIST){return true;}
+	}
 
-	function getNewsNum(){return count($this->_data["stories"]);}
+	function getDate(){
+		if($this->isList()){
+			return $this->_data['date'];
+		}
+	}
+
+	function getNewsNum(){
+		if($this->isList()){
+			return count($this->_data["stories"]);
+		}
+	}
 
 	function getStories(){
-		$this->storiesData=$this->_data["stories"];
-		return $this->storiesData;
+		if($this->isList()){
+			$this->storiesData=$this->_data["stories"];
+			return $this->storiesData;
+		}
 	}
 
-	function getSingleNew($key){
-		$this->singleNew=$this->storiesData[$key];
+	function getSingleNew($key=NULL){
+		if($this->isList()){
+			if(!$key==NULL){
+				$this->singleNew=$this->storiesData[$key];
+			}
+		}else{
+			$this->singleNew=$this->_data;
+		}
 		return $this->singleNew;
 	}
+
+	function getImageSource(){return $this->singleNew['image_source'];}
 
 	function getTitle(){return $this->singleNew['title'];}
 
@@ -118,23 +147,161 @@ class DataObj{
 	function getGaOrefix(){return $this->singleNew['ga_prefix'];}
 
 	function getImages(){
-    	$pic=$this->singleNew['images'][0];//远程文件路径
-        $data=file_get_contents($pic); // 读文件内容 
+		if($this->method==self::_LIST){
+			$namePrefix="";
+			$pic=$this->singleNew["images"][0];//远程文件路径
+		}else{
+			$namePrefix="body_";
+			$pic=$this->singleNew["image"];
+		}
+    	
         $filepath="../img/".$this->getDate()."/";
         if(!is_dir($filepath)){
             mkdir($filepath,0777,true);
         }
-        $filename=$this->getID().'.'.substr($pic,-3,3);
-        $fp=@fopen($filepath.$filename,"w"); 
-        @fwrite($fp,$data);
-        fclose($fp);
-		return $filepath.$filename;
+        $filename=$namePrefix.$this->getID().'.'.substr($pic,-3,3);
+        if(!file_exists($filepath.$filename)){
+        	$data=file_get_contents($pic); // 读文件内容 
+        	$fp=@fopen($filepath.$filename,"w"); 
+       		@fwrite($fp,$data);
+        	fclose($fp);
+        }
+        return $filepath.$filename;
 	}
 
 	function getType(){return $this->singleNew['type'];}
 
 	function getID(){return $this->singleNew['id'];}
 
+	function getCSS(){return $this->singleNew['css'][0];}
+
+	function getBody(){return $this->singleNew['body'];}
+
+}
+
+class dateMgr{
+
+	function __construct(){date_default_timezone_set('UTC');}
+
+	//判断是不是合法月份
+	function isMonth($mon){
+		$montharr=array(1,2,3,4,5,6,7,8,9,10,11,12);
+		$flag=0;
+		foreach ($montharr as $key => $value) {
+			if($mon==$value){
+				$flag++;
+			}
+		}
+		if($flag>0){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	//打印出某年某月的天数
+	function printDate($year,$mon){
+		//20130520
+		$montharr=array(1,2,3,4,5,6,7,8,9,10,11,12);
+		$stand = "$year-$mon-";
+
+  		//取得每个月的天数
+		foreach ($montharr as $key => $monthv) {
+			$daysarr[$key+1]=$this->getMonthLastDay($monthv,$year);
+		}
+
+		$tmp_mon=substr($mon,0,1);
+		if($tmp_mon=="0"){
+			$tmp_mon=substr($mon,1,1);
+		}
+
+		for ($i=1;$i<=$daysarr[$tmp_mon];$i++) {  
+    		$time=strtotime($stand.$i);  
+    		$date[]=date("Ymd",$time);  
+		}  
+  
+		return $date;
+	}
+
+	function getMonthLastDay($month,$year){
+		$nextMonth=(($month+1)>12) ? 1 : ($month+1);
+		$year= ($nextMonth>12) ? ($year+1) : $year;
+		$lastDay=date('d',mktime(0,0,0,$nextMonth,0,$year));
+		return $lastDay;
+	}
+
+
+	function getAllDate(){
+		$nowTime=time();
+
+		$unixtimestamp=strtotime("2013-05-20");
+
+		$days=round(($nowTime-$unixtimestamp)/3600/60);
+
+		$timeWithoutUnix=date("Y-m-d",time());
+		$timeWithoutUnix_exploded=explode("-",$timeWithoutUnix);
+		$timeWithoutUnix="";
+		foreach ($timeWithoutUnix_exploded as $key => $value) {
+			$timeWithoutUnix.=$value;
+		}
+
+		//获得20130520到0531的天数,因为知乎日报开始自20130520
+		$march2013=$this->printDate("2013","05");
+		$march2013_fixed=array();
+		for ($i=19; $i <=30; $i++) { 
+			$march2013_fixed[]=$march2013[$i];
+		}
+
+		//获得2013年剩下的天数
+		$mon2013=array();
+
+		for ($i=6; $i <=12; $i++) { 
+			$mon2013[]=$this->printDate("2013",$i);
+		}
+
+		//获得2014年及其以后的天数
+		$monOther=array();
+		for ($i=2014; $i <= $timeWithoutUnix_exploded[0]; $i++) { 
+			for ($j=1; $j <=$timeWithoutUnix_exploded[1]; $j++) { 
+				$monOther[]=$this->printDate($i,$j);
+			}
+		}
+
+		$monOther_fixed=array();
+
+		//剔除当月的多余部分
+		array_splice($monOther[count($monOther)-1],0,($this->getMonthLastDay($timeWithoutUnix_exploded[1],$timeWithoutUnix_exploded[0])-$timeWithoutUnix_exploded[2]));
+
+		foreach ($monOther[count($monOther)-1] as $key => $value) {
+			$monOther[count($monOther)-1][$key]=$value-($this->getMonthLastDay($timeWithoutUnix_exploded[1],$timeWithoutUnix_exploded[0])-date("d"));
+		}
+
+		//将多个数组混合到一起
+		$totalMon=array_merge($march2013_fixed,$mon2013,$monOther);
+
+		//释放内存
+		$march2013=array();
+		$march2013_fixed=array();
+		$mon2013=array();
+		$monOther=array();
+
+		//将二维数组转换为一维数组
+		foreach ($totalMon as $key => $value) {
+			if(is_array($value)){
+				foreach ($value as $key2 => $value2) {
+					$intervalDate[]=$value2;
+				}
+			}else{
+				$intervalDate[]=$value;
+			}
+		}
+
+		$totalMon=array();
+
+		$intervalDate=array_reverse($intervalDate);
+
+		return $intervalDate;
+	}
 }
 
 //ini
@@ -147,7 +314,7 @@ $jsonData=$tm->getJSON($gb);
 $jsonArray=$tm->getContext(new decodeJSON($jsonData));
 
 //print_r($jsonArray);
-$dataParser=new DataObj($jsonArray);
+$dataParser=new DataObj($jsonArray,DataObj::_LIST);
 //echo $dataParser->getDate();
 
 $stories=$dataParser->getStories();
@@ -172,5 +339,12 @@ $jsonData=$tm->getJSON($gb,$urlAvailable);
 $jsonArray=$tm->getContext(new decodeJSON($jsonData));
 
 //print_r($jsonArray);
+$dataParser=new DataObj($jsonArray,DataObj::BODY);
+$stories=$dataParser->getSingleNew();
+//echo "<img src=\"".$dataParser->getImages()."\" alt=\"233\"/>";
+//echo $dataParser->getBody();
+
+$allDate=new dateMgr();
+//print_r($allDate->getAllDate());
 
 ?>
---
+++ beta/index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+ 	<head>
+ 		<title>直呼日报 2.0 Beta</title>
+ 		<link href="style/style.css" rel="stylesheet">
+ 		<meta http-equiv="Content-Type" content="text/html; charset=utf8" />
+ 		<meta name="Description" content="直呼日报">
+		<meta name="keywords" content="直呼日报,每天⑨次,干杯,如何正确的吐槽,知乎,知识,好奇心" />
+ 	</head>
+
+ 	<body>
+		<div class="body-head">
+			<div class="body-name">
+				直呼日报 Beta
+				<p class="help-block">每天⑨次,⑨次每天</p>
+			</div>
+			<div class="body-menu">
+				<ul class="main-menu">
+					<li class="menu-logo"><a href=""><img src="favicon.ico" alt="logo" /></a></li>
+					<li><a href="">日报首页</a></li>
+					<li><a href="">过往消息</a></li>
+					<li><a href="">联系我</a></li>
+					<li><a href="">API</a></li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="body-content">
+			<div class="content-main-box">
+				<div class="main-box-img">
+					<img src="http://hd.shijue.cvidea.cn/tf/140919/2351836/541bb4363dfae98c13000001.JPEG" alt="11" />
+				</div>
+				<div class="main-box-content">
+					<div class="main-title">
+						<a href="">我参与了很多大公司的上市敲钟，分享下现场的图片</a>
+					</div>
+					<div class="main-time">20140520</div>
+				</div>
+				<div class="main-box-footer">
+					<div class="main-box-postscript">来自:<a href="http://zhihu.com">zhihu.com</a></div>
+				</div>
+			</div>
+
+			<div class="content-main-box">
+				<div class="main-box-img">
+					<img src="http://hd.shijue.cvidea.cn/tf/140919/2351836/541bb4363dfae98c13000001.JPEG" alt="11" />
+				</div>
+				<div class="main-box-content">
+					<div class="main-title">
+						<a href="">我参与了很多大公司的上市敲钟，分享下现场的图片</a>
+					</div>
+					<div class="main-time">20140520</div>
+				</div>
+				<div class="main-box-footer">
+					<div class="main-box-postscript">来自:<a href="http://zhihu.com">zhihu.com</a></div>
+				</div>
+			</div>
+
+			<div class="content-main-box">
+				<div class="main-box-img">
+					<img src="http://hd.shijue.cvidea.cn/tf/140919/2351836/541bb4363dfae98c13000001.JPEG" alt="11" />
+				</div>
+				<div class="main-box-content">
+					<div class="main-title">
+						<a href="">我参与了很多大公司的上市敲钟，分享下现场的图片</a>
+					</div>
+					<div class="main-time">20140520</div>
+				</div>
+				<div class="main-box-footer">
+					<div class="main-box-postscript">来自:<a href="http://zhihu.com">zhihu.com</a></div>
+				</div>
+			</div>
+
+			<div class="content-main-box">
+				<div class="main-box-img">
+					<img src="http://hd.shijue.cvidea.cn/tf/140919/2351836/541bb4363dfae98c13000001.JPEG" alt="11" />
+				</div>
+				<div class="main-box-content">
+					<div class="main-title">
+						<a href="">我参与了很多大公司的上市敲钟，分享下现场的图片</a>
+					</div>
+					<div class="main-time">20140520</div>
+				</div>
+				<div class="main-box-footer">
+					<div class="main-box-postscript">来自:<a href="http://zhihu.com">zhihu.com</a></div>
+				</div>
+			</div>
+
+			<div class="content-main-box">
+				<div class="main-box-img">
+					<img src="http://hd.shijue.cvidea.cn/tf/140919/2351836/541bb4363dfae98c13000001.JPEG" alt="11" />
+				</div>
+				<div class="main-box-content">
+					<div class="main-title">
+						<a href="">我参与了很多大公司的上市敲钟，分享下现场的图片</a>
+					</div>
+					<div class="main-time">20140520</div>
+				</div>
+				<div class="main-box-footer">
+					<div class="main-box-postscript">来自 <a href="http://zhihu.com">zhihu.com</a></div>
+				</div>
+			</div>
+
+			<div class="content-main-box">
+				<div class="main-box-img">
+					<img src="http://hd.shijue.cvidea.cn/tf/140919/2351836/541bb4363dfae98c13000001.JPEG" alt="11" />
+				</div>
+				<div class="main-box-content">
+					<div class="main-title">
+						<a href="">我参与了很多大公司的上市敲钟，分享下现场的图片</a>
+					</div>
+					<div class="main-time">20140520</div>
+				</div>
+				<div class="main-box-footer">
+					<div class="main-box-postscript">来自 <a href="http://zhihu.com">zhihu.com</a></div>
+				</div>
+			</div>
+		</div>
+ 	</body>
+
+ 	<foot>
+
+ 	</foot>
+
+</html>
