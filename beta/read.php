38c337359cb00b49a7fe9ea445dd7552233972a3
ivy <597055914@qq.com>
2014-09-24 23:29:14 +0800

    update

--- beta/read.php
+++ beta/read.php
@@ -22,22 +22,25 @@ if(strlen($id)==0 || !is_numeric($id)){
 		header("location:index.php");
 	}else{
 		//print_r($stories);
-		//echo "<img src=\"".$dataParser->getImages()."\">";
+		//echo $dataParser->getImages();
 ?>
 		<div class="read-content">
 			<div class="read-left">
 				<div class="read-title">
-					<p>羽翼</p>
+					<div class="read-headline">羽翼</div>
 					<div class="help-info">
-						<ul>
-							<li>1</li>
-							<li>2</li>
-						</ul>
+						<p>日期：20100520 图片来源：蛤蛤蛤</p>
 					</div>
+				</div>
+				<div class="read-main-content">
+					sdsdsddsd
+				</div>
+				<div class="read-footer">
+					<span class="read-copyright">版权声明：除非注明，本站文章均为原创或编译，转载请注明： 文章来自 <a href="http://zhihu.com" target="_blank">知乎</a></span>
 				</div>
 			</div>
 			<div class="read-right">
-				
+				<img src="<?php echo $dataParser->getImages(); ?>" alt="125152" width="339" height="200">
 			</div>
 		</div>
 <?php
